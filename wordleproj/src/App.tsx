82e37aae318c14a584df311d5cfdf29c1d474018
Sasha Xox <sasha_xox@tut.by>
2024-03-06 19:39:56 +0300

    tailwind added

---
+++ wordleproj/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="mx-auto w-96">
+      <h1 className="text-4xl text-center">Wordle</h1>
+    </div>
+  );
+}
+
+export default App;
