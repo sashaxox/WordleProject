495900e4e7ff07e3396c44783568a811d75c4f20
Sasha Xox <sasha_xox@tut.by>
2024-03-07 21:21:02 +0300

    complited project

--- wordleproj/src/App.tsx
+++ wordleproj/src/App.tsx
@@ -1,12 +1,159 @@
-import "./App.css";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect,  useState } from 'react';
+import Keyboard from './Keyboard';
+import { useStore, NUMBER_OF_GUESSES, WORD_LENGTH } from './store';
+import { isValidWord } from './word-utils';
+import WordRow from './WordRow';
+import { usePrevious } from "@uidotdev/usehooks"
 
-function App() {
+export default function App() {
+  const state = useStore();
+  const [guess, setGuess, addGuessLetter] = useGuess();
+
+  const [showInvalidGuess, setInvalidGuess] = useState(false);
+  useEffect(() => {
+    let id: ReturnType<typeof setTimeout>;
+    if (showInvalidGuess) {
+      id = setTimeout(() => setInvalidGuess(false), 1500);
+    }
+
+    return () => clearTimeout(id);
+  }, [showInvalidGuess]);
+
+  const addGuess = useStore((s) => s.addGuess);
+  const previousGuess = usePrevious(guess);
+  useEffect(() => {
+    if (guess.length === 0 && previousGuess?.length === WORD_LENGTH) {
+      if (isValidWord(previousGuess)) {
+        setInvalidGuess(false);
+        addGuess(previousGuess);
+      } else {
+        setInvalidGuess(true);
+        setGuess(previousGuess);
+      }
+    }
+  }, [guess]);
+
+  const isGameOver = state.gameState !== 'playing';
+
+  let rows = [...state.rows];
+
+  let currentRow = 0;
+  if (rows.length < NUMBER_OF_GUESSES) {
+    currentRow = rows.push({ guess }) - 1;
+  }
+
+  const guessesRemaining = NUMBER_OF_GUESSES - rows.length;
+
+  rows = rows.concat(Array(guessesRemaining).fill(''));
 
   return (
-    <div className="mx-auto w-96">
-      <h1 className="text-4xl text-center">Wordle</h1>
+    <div className="mx-auto w-96 relative h-screen">
+      <header className="border-b border-gray-400 py-4">
+        <h1 className="text-3xl font-bold text-center uppercase">Reacdle</h1>
+      </header>
+
+      <main className="grid grid-rows-6 gap-4 my-4">
+        {rows.map((word, index) => (
+          <WordRow
+            key={index}
+            word={word.guess}
+            result={word.result}
+            className={
+              showInvalidGuess && index === currentRow ? 'animate-bounce' : ''
+            }
+          />
+        ))}
+      </main>
+
+      <Keyboard
+        onClick={(key) => {
+          addGuessLetter(key);
+        }}
+      />
+
+      {isGameOver && (
+        <div
+          role="modal"
+          className="absolute bg-white border border-gray-500 rounded text-center
+            w-11/12 h-1/2 p-6 left-0 right-0 mx-auto top-1/4
+           grid grid-rows-4"
+        >
+          <p>Game Over</p>
+          <WordRow
+            word={state.answer}
+            className="items-center justify-items-center"
+          />
+
+          <button
+            className="border border-green-500 rounded bg-green-500 p-2 mt-4 text-gray-800 shadow"
+            onClick={() => {
+              state.newGame();
+              setGuess('');
+            }}
+          >
+            New Game
+          </button>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+function useGuess(): [
+  string,
+  React.Dispatch<React.SetStateAction<string>>,
+  (letter: string) => void
+] {
+  const [guess, setGuess] = useState('');
+
+  const addGuessLetter = (letter: string) => {
+    setGuess((curGuess) => {
+      const newGuess =
+        letter.length === 1 && curGuess.length !== WORD_LENGTH
+          ? curGuess + letter
+          : curGuess;
+
+      switch (letter) {
+        case 'Backspace':
+          return newGuess.slice(0, -1);
+        case 'Enter':
+          if (newGuess.length === WORD_LENGTH) {
+            return '';
+          }
+      }
+
+      if (newGuess.length === WORD_LENGTH) {
+        return newGuess;
+      }
+
+      return newGuess;
+    });
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    const letter = e.key;
+    addGuessLetter(letter);
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, []);
+
+  return [guess, setGuess, addGuessLetter];
+}
+
+// source https://usehooks.com/usePrevious/
+// function usePrevious<T>(value: T): T {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const ref: any = useRef<T>();
+
+//   useEffect(() => {
+//     ref.current = value;
+//   }, [value]);
+
+//   return ref.current;
+// }
